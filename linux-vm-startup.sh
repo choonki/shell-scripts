3311e55f1557dab2b26f2c7a365a3ca9667f6356
Jo Choonki <35411588+choonki@users.noreply.github.com>
2023-05-22 16:59:39 +0900

    Update linux-vm-startup.sh

--- linux-vm-startup.sh
+++ linux-vm-startup.sh
@@ -3,7 +3,7 @@
 cd /root
 
 aliases=$(curl -sL --fail https://raw.githubusercontent.com/choonki/shell-aliases/main/.bash_aliases)
-
 if [ ! -z "${aliases}" ]; then
   echo "${aliases}" > .bash_aliases
 fi
+
